f13239e2632c7ddd15008baa31fa934c4206c862
Adam Sliwakowski <sliwakowski@gmail.com>
2016-04-22 21:39:32 +0200

    Created models

---
+++ Succotash/Succotash/Model/Question.swift
@@ -0,0 +1,12 @@
+//
+//  Question.swift
+//  Succotash
+//
+//  Created by Adam Śliwakowski on 22.04.2016.
+//  Copyright © 2016 sliwakowski. All rights reserved.
+//
+
+struct Question {
+    var text: String
+    var image: String
+}
---
+++ Succotash/Succotash/Model/QuestionsBlock.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionsBlock.swift
+//  Succotash
+//
+//  Created by Adam Śliwakowski on 22.04.2016.
+//  Copyright © 2016 sliwakowski. All rights reserved.
+//
+
+struct QuestionsBlock {
+    var isMandatory: Bool
+    var isFinished: Bool = false
+    var questions: [Question]
+}
