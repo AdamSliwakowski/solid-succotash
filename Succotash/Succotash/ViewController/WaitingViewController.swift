cc8f8657c398bdc4d3582f4047d1562fe84fd017
Adam Sliwakowski <sliwakowski@gmail.com>
2016-04-22 23:58:05 +0200

    Added timer to count time and update label

--- Succotash/Succotash/ViewController/WaitingViewController.swift
+++ Succotash/Succotash/ViewController/WaitingViewController.swift
@@ -12,15 +12,35 @@ import SwiftyTimer
 class WaitingViewController: UIViewController {
     
     @IBOutlet weak var timeLeftLabel: UILabel!
+    private var timer: NSTimer!
+    private var timeLeft = 80 {
+        didSet {
+            configureTimeLeftText()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTimer()
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
     private func configureTimer() {
-        NSTimer.after(3.seconds) {
+        NSTimer.after(10.seconds) {
             self.dismissViewControllerAnimated(true, completion: nil)
         }
+        timer = NSTimer.every(1.second) {
+            self.timeLeft = self.timeLeft - 1
+        }
+    }
+    
+    private func configureTimeLeftText() {
+        let minutes = Int(timeLeft / 60)
+        let seconds = timeLeft - (minutes * 60)
+        timeLeftLabel.text = String(format: "%02d", minutes) + ":" + String(format: "%02d", seconds)
     }
 }
