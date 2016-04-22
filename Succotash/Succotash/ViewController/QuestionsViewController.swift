2907d023e9d4f80defb88e304716f3e7c41d1522
Adam Sliwakowski <sliwakowski@gmail.com>
2016-04-22 22:30:32 +0200

    Created first version of QuestionsVC

---
+++ Succotash/Succotash/ViewController/QuestionsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionsViewController.swift
+//  Succotash
+//
+//  Created by Adam Śliwakowski on 22.04.2016.
+//  Copyright © 2016 sliwakowski. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+    
+}
