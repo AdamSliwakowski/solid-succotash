f0f7d73726cc4ea024df716d7fa9cdcbfc0a375d
Adam Sliwakowski <sliwakowski@gmail.com>
2016-04-22 22:49:06 +0200

    First version of handling collection view to populate questions

--- Succotash/Succotash/View/QuestionCell.swift
+++ Succotash/Succotash/View/QuestionCell.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016 sliwakowski. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class QuestionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func configure(question: Question) {
+        configureAppearance()
+        textLabel.text = question.text
+        imageView.image = UIImage(named: question.image)
+    }
+    
+    private func configureAppearance() {
+        layer.cornerRadius = 10
+        layer.borderColor = UIColor(white: 0.85, alpha: 1).CGColor
+        layer.borderWidth = 1
+    }
+}
--- Succotash/Succotash/ViewController/QuestionsViewController.swift
+++ Succotash/Succotash/ViewController/QuestionsViewController.swift
@@ -7,7 +7,33 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class QuestionsViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    var questionsBlock: QuestionsBlock!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionsBlock = JSON.read("questions")["blocks"].arrayValue.map { QuestionsBlock(json: $0) }.first!
+    }
+}
+
+
+extension QuestionsViewController: UICollectionViewDataSource {
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return questionsBlock.questions.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("QuestionCell", forIndexPath: indexPath) as! QuestionCell
+        cell.configure(questionsBlock.questions[indexPath.row])
+        return cell
+    }
 }
